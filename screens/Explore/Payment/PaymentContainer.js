517882de33fbc1258806664a6ce2b9383b4193a4
김지영 <rlawldud335@naver.com>
2021-05-30 09:04:47 +0900

    Payment

--- screens/Explore/Payment/PaymentContainer.js
+++ screens/Explore/Payment/PaymentContainer.js
@@ -1,25 +1,91 @@
-import React, { useEffect } from "react";
-import { View, Text } from "react-native";
+import Api from "../../../api";
+import PaymentPresenter from "./PaymentPresenter";
+import React, { useState, useEffect } from "react";
+import {
+  ActivityIndicator,
+  View,
+  TouchableOpacity,
+  Text,
+  Alert,
+} from "react-native";
 
-export default ({ route }) => {
-  console.log(route.params);
+export default ({ route, navigation }) => {
+  const {
+    params: { challengeData: cData },
+  } = route;
 
-  const getData = () => {};
+  const [challengeData, setChallengeData] = useState();
+
+  const getData = async () => {
+    const response = await Api.getChallengeChallengeId(cData.challengeId);
+    if (response.status == 200) {
+      setChallengeData(response.data);
+    } else {
+      Alert.alert("500 error");
+    }
+  };
 
   useEffect(() => {
     getData();
   }, []);
 
-  return (
+  return challengeData ? (
+    <View style={{ flex: 1, backgroundColor: "white" }}>
+      <View
+        style={{
+          height: "93%",
+          width: "100%",
+        }}
+      >
+        <PaymentPresenter challenge={challengeData} navigation={navigation} />
+      </View>
+
+      <View
+        style={{
+          height: "7%",
+          width: "100%",
+          position: "absolute",
+          bottom: 0,
+          backgroundColor: "white",
+          borderTopColor: "#CACACA",
+          borderTopWidth: 0.2,
+          flexDirection: "row",
+          justifyContent: "space-around",
+          alignItems: "center",
+        }}
+      >
+        <TouchableOpacity
+          style={{
+            width: "60%",
+            minWidth: 300,
+            backgroundColor: "#FF5E5E",
+            height: 40,
+            borderRadius: 10,
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+          onPress={() => {
+            navigation.navigate("payment");
+          }}
+        >
+          <Text
+            style={{ color: "white", fontFamily: "nanumBold", fontSize: 17 }}
+          >
+            결제하기
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  ) : (
     <View
       style={{
         flex: 1,
-        alignItems: "center",
-        justifyContent: "center",
+        justifyContents: "center",
+        aligenItem: "center",
         backgroundColor: "white",
       }}
     >
-      <Text>Payment</Text>
+      <ActivityIndicator />
     </View>
   );
 };
---
+++ screens/Explore/Payment/PaymentPresenter.js
@@ -0,0 +1,235 @@
+import React from "react";
+import Api from "../../../api";
+import styled from "styled-components/native";
+import Title from "../../../components/Title";
+import Tag from "../../../components/Tag";
+import RedButton from "../../../components/RedButton";
+import { View, Image, Platform, TouchableOpacity } from "react-native";
+
+export default ({ challenge }) => {
+  return (
+    <SView>
+      <Contents>
+        <Title title={"챌린지 정보"} />
+        <ProofWrap>
+          <View
+            style={{ flexDirection: "row", justifyContent: "space-around" }}
+          >
+            <Image
+              source={{ uri: challenge.challengeTitleImage }}
+              style={{
+                width: Platform.OS === "web" ? 200 : 100,
+                height: 130,
+                marginBottom: 10,
+                marginRight: 20,
+              }}
+            />
+
+            <View
+              style={{
+                flexDirection: "column",
+                justifyContent: "space-around",
+              }}
+            >
+              <BigTitle>{challenge.challengeTitle}</BigTitle>
+              <TagWrap>
+                <Tag tagName={`${challenge.deposit} P`} />
+                <Tag tagName={`${challenge.proofAvailableDay}`} />
+                <Tag tagName={`하루 ${challenge.proofCount}번 인증`} />
+                <Tag tagName={`하루 ${challenge.proofCountOneDay}번 인증`} />
+                <Tag tagName={`${challenge.challengeCategory}`} />
+                {challenge.licenseName && (
+                  <Tag tagName={`${challenge.licenseName}`} />
+                )}
+              </TagWrap>
+            </View>
+          </View>
+
+          <View
+            style={{
+              borderBottomColor: "#9F9F9F",
+              borderBottomWidth: 1,
+            }}
+          />
+
+          <View>
+            <View
+              style={{
+                flexDirection: "row",
+                justifyContent: "space-between",
+                margin: 15,
+              }}
+            >
+              <Middle>참가 기간</Middle>
+              <Middle>
+                {challenge.chgStartDt.slice(0, 10)} ~{" "}
+                {challenge.chgEndDt.slice(0, 10)}
+              </Middle>
+            </View>
+
+            <View
+              style={{
+                flexDirection: "row",
+                justifyContent: "space-between",
+                margin: 15,
+              }}
+            >
+              <Middle>참가 포인트</Middle>
+              <Middle>{challenge.deposit}P</Middle>
+            </View>
+
+            <View
+              style={{
+                flexDirection: "row",
+                justifyContent: "space-between",
+                margin: 15,
+              }}
+            >
+              <Middle>참가 인원</Middle>
+              <Middle>
+                {challenge.joinPeople}/{challenge.limitPeople}명
+              </Middle>
+            </View>
+          </View>
+        </ProofWrap>
+
+        <Title title={"참가 포인트 결제"} />
+        <IntroWrap>
+          <View>
+            <View
+              style={{
+                flexDirection: "row",
+                justifyContent: "space-between",
+                margin: 15,
+              }}
+            >
+              <Middle>현재 포인트</Middle>
+              <PinkLine>
+                <PinkText>200</PinkText>
+                <Middle>포인트</Middle>
+              </PinkLine>
+            </View>
+
+            <View
+              style={{
+                flexDirection: "row",
+                justifyContent: "space-between",
+                margin: 15,
+              }}
+            >
+              <Middle>결제 포인트</Middle>
+              <PinkLine>
+                <PinkText>{-challenge.deposit}</PinkText>
+                <Middle>포인트</Middle>
+              </PinkLine>
+            </View>
+
+            <View
+              style={{
+                flexDirection: "row",
+                justifyContent: "space-between",
+                margin: 15,
+              }}
+            >
+              <Middle>결제 후 포인트</Middle>
+              <PinkLine>
+                <PinkText>{200 - challenge.deposit}</PinkText>
+                <Middle>포인트</Middle>
+              </PinkLine>
+            </View>
+          </View>
+
+          <TouchableOpacity
+            style={{
+              width: "40%",
+              minWidth: 300,
+              backgroundColor: "#CE9FFF",
+              height: 30,
+              borderRadius: 10,
+              justifyContent: "center",
+              alignItems: "center",
+              alignContent: "center",
+            }}
+            onPress={() => {
+              navigation.navigate(""); // 포인트 충전하는 화면으로 이동
+            }}
+          >
+            <Text
+              style={{
+                color: "#3b1464",
+                fontFamily: "nanumBold",
+                fontSize: 15,
+              }}
+            >
+              부족한 포인트 충전하기
+            </Text>
+          </TouchableOpacity>
+        </IntroWrap>
+      </Contents>
+    </SView>
+  );
+};
+
+const SView = styled.ScrollView``;
+
+const BigTitle = styled.Text`
+  font-family: "nanumBold";
+  font-size: 24px;
+  color: #3b1464;
+  text-align: center;
+  margin-bottom: 10px;
+`;
+
+const Middle = styled.Text`
+  font-family: "nanumBold";
+  font-size: 17px;
+  color: #3b1464;
+`;
+
+const PinkText = styled.Text`
+  font-family: "nanumBold";
+  font-size: 17px;
+  color: #ff5e5e;
+`;
+
+const PinkLine = styled.View`
+  width: 100px;
+  padding: 5px;
+  border-bottom-width: 3px;
+  border-bottom-color: #ff5e5e;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+const ProofWrap = styled.View`
+  background-color: #ffc0c0;
+  padding: 10px;
+  border-radius: 10px;
+  margin-top: 10px;
+  margin-bottom: 40px;
+`;
+
+const IntroWrap = styled.View`
+  margin-top: 10px;
+  background-color: #eeeeee;
+  border-radius: 10px;
+  padding: 10px;
+`;
+
+const Text = styled.Text`
+  font-family: "nanumBold";
+  font-size: 17px;
+  color: #3b1464;
+`;
+
+const Contents = styled.View`
+  padding: 20px;
+`;
+
+const TagWrap = styled.View`
+  width: 200px;
+  flex-direction: row;
+  flex-wrap: wrap;
+  margin-bottom: 10px;
+  margin-right: 10px;
+`;
